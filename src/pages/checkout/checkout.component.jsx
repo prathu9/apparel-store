df18f710d8bea1901ed8c6d225a71642f5ba5d0a
prathamesh <prathameshpgs@gmail.com>
2021-03-20 20:27:55 +0530

    Integrated stripe payment service and update favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,7 +3,9 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -35,6 +37,12 @@ const CheckoutPage = ({cartItems,total})=>{
                 />)
             }
             <div className="total">Total: ${total}</div>
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br/>
+                4242 4242 4242 4242 - Exp: 01/02 - CVV: 123
+            </div>
+            <StripeCheckoutButton />
         </div>
     )
 }
