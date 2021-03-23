cc850ff7a47f888549ea393739734727ff88a2b7
prathamesh <prathameshpgs@gmail.com>
2021-03-23 22:28:46 +0530

    testing

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,10 +7,9 @@ import rootReducer from "./root.reducer";
 
 const middlewares = [];
 
-if(process.env.NODE_ENV === 'development'){
-	console.log("test");
-	middlewares.push(logger);
-}
+// if(process.env.NODE_ENV === 'development'){
+// 	middlewares.push(logger);
+// }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
