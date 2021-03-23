9fc0a7c14309105f114648654fe26500d7ff1bb3
prathamesh <prathameshpgs@gmail.com>
2021-03-23 22:05:26 +0530

    Removed console log

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -9,7 +9,6 @@ import CollectionPreview from '../collection-preview/collection-preview.componen
 import './collection-overview.style.scss';
 
 const CollectionOverview = ({collections})=>{
-	console.log(collections);
 	return(
 		<div className = "collections-overview">
 				{
