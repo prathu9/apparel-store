7c5f5dbfabd5d6fea13d6353de3cd70c2b0f5267
prathamesh <prathameshpgs@gmail.com>
2021-03-24 21:16:11 +0530

    Bug fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,7 @@ import {persistStore} from 'redux-persist';
 import rootReducer from "./root.reducer";
 
 
-const middlewares = [];
-
-if(process.env.NODE_ENV === 'development'){
-	console.log(process.env.NODE_ENV);
-	middlewares.push(logger);
-}
+const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
