86289ce07e61ea449d3001e43216b280e86930b3
prathamesh <prathameshpgs@gmail.com>
2021-04-16 23:27:39 +0530

    Cart dropdown checkout button link fixed.

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -22,7 +22,7 @@ const CartDropdown = ({cartItems, history, dispatch})=>{
                     />):<span className="empty-message">Your cart is empty</span>
                 }
             </div>
-            <CustomButton onClick={()=>{history.push("./checkout"); 
+            <CustomButton onClick={()=>{history.push("/checkout"); 
                                     dispatch(toggleCartHidden())}
                                   }>
                 Go To Checkout
