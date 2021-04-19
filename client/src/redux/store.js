2388349fdf21ab2e824ac922a2dc925d32321231
prathamesh <prathameshpgs@gmail.com>
2021-04-19 21:15:58 +0530

    Reverted earlier changes in store and added compose from redux to be used instead of composeWithDevTools in production.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
@@ -7,17 +7,19 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "./root.reducer";
 import rootSaga from './root-saga';
 
-const composeEnhancers = composeWithDevTools({
+const nodeEnv = process.env.NODE_ENV;
+
+const composeEnhancers = nodeEnv !== 'production'? composeWithDevTools({
 	// Specify name here, actionsBlacklist, actionsCreators and other options if needed
-  });
+  }) : compose;
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [sagaMiddleware, logger];
+const middlewares = [sagaMiddleware];
 
-// if(process.env.NODE_ENV === 'development'){
-// 	middlewares.push(logger);
-// }
+if(nodeEnv === 'development'){
+	middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, composeEnhancers(
 	applyMiddleware(...middlewares),
