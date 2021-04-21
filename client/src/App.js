129e19b025f7e6378654fc4aa9a0cb42f9b55f62
prathamesh <prathameshpgs@gmail.com>
2021-04-21 20:46:14 +0530

    Adding performance optimization with react lazy and suspense, also added Error boundary for error handling.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {createStructuredSelector} from 'reselect';
 
 import Header from './components/header/header.component';
 import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
 import {selectCurrentUser} from './redux/user/user.selectors';
 import {checkUserSession} from './redux/user/user.action';
@@ -29,20 +30,22 @@ const App = ({checkUserSession, currentUser}) => {
       <GlobalStyle/>
       <Header/>
       <Switch>
-        <Suspense fallback={<Spinner/>}>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/shop" component={ShopPage}/>
-            <Route exact path="/checkout" component={CheckoutPage}/> 
-            <Route 
-              exact 
-              path="/signin" 
-              render={()=>(
-                currentUser?(
-                <Redirect to='/' />
-              ):(
-                <SignInAndSignUpPage/>
-              ))}/>
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner/>}>
+              <Route exact path="/" component={HomePage}/>
+              <Route path="/shop" component={ShopPage}/>
+              <Route exact path="/checkout" component={CheckoutPage}/> 
+              <Route 
+                exact 
+                path="/signin" 
+                render={()=>(
+                  currentUser?(
+                  <Redirect to='/' />
+                ):(
+                  <SignInAndSignUpPage/>
+                ))}/>
           </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
